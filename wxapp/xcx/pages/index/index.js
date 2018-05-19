16a4817ceed7afc1f4d5961fdb322ef3f9a419f9
zll18314831310 <18314831310@163.com>
2018-05-19 20:13:39 +0800

    上传了MVVM数据方面

--- wxapp/xcx/pages/index/index.js
+++ wxapp/xcx/pages/index/index.js
@@ -1,54 +1,85 @@
 //index.js
 //获取应用实例
+import util from '../../utils/index';
+import config from '../../utils/config';
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    hiddenLoading: false,
+    hasMore: true,
+    page: 1,
+    days: 3,
+    pageSize: 4,
+    articleList:[]
+
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad() {
+    this.requestArticle();
+  },
+  requestArticle(){
+    util.request({
+      url: 'list',
+      // ?有可能来自于远程的web url， mock 
+      mock: true,
+      data: {
+        tag: '微信热门',
+        start: this.data.page || 1,
+        days: this.data.days || 3,
+        pageSize: this.data.pageSize,
+        langs: 'en'
+      }
+
+    }).then(res=>{
+      console.log(res)
+      this.setData({
+        hiddenLoading: true,
+        articleList: res.data
+      })
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  onShareAppMessage() {
+    let title = config.defaultShareText || ''
+    return {
+      title,
+      path: `/pages/index/index`,
+      success: function(res) {
+        
+      },
+      fail: function(res) {
+
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onReachBottom() {
+    if(this.data.hasMore) {
+      let nextPage = this.data.page +1;
+      this.setData({
+        page: nextPage
+      })
+      this.requestArticle();
+    }
+  },
+  onPullDownRefresh() {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hiddenLoading: false
+    })
+    this.requestArticle();
+  },
+  showDetail(e) {
+    let dataset = e.currentTarget.dataset;
+    let item = dataset && dataset.item
+    let contentId = item.contentId || 0;
+    wx.navigateTo({
+      url: `../detail/detail?contentId=${contentId}`
     })
   }
-})
+  
+  
+  
+  
+  
+  
+  
+  })
