387fd015db9419e424f00424fbfb807561ed3eab
zll18314831310 <18314831310@163.com>
2018-06-05 21:21:17 +0800

    mall－wepy学习

--- wxapp/mall/src/utils/wxRequest.js
+++ wxapp/mall/src/utils/wxRequest.js
@@ -2,8 +2,17 @@
 // 函数 url, params, get, POST
 // @params json query 传查询参数 method 方法
 import wepy from 'wepy';
+import util from '@/utils/util';
+import md5 from '@/utils/md5';
+const API_SECREY_KEY = 'www.mall.cycle.com';
+const TIMESTAMP = util.getCurrentTime();
+const SIGN = md5.hex_md5((TIMESTAMP+API_SECREY_KEY).toLowerCase());
 const wxRequest = async(params ={},url) => {
-    let data = params.query || {}
+    let data = params.query || {};
+    // 在请求的过程中带上时间戳常用的做法
+    // 加密 code 你有，md5 加密
+    data.sign = SIGN;
+    data.time = TIMESTAMP;
     let res = await wepy.request({
         url: url,
         method: params.method || 'GET',
